5c36e5d3b9cbf64d34d5fd175155e877e438d9d8
Ziqian Li <liziqian1992@gmail.com>
2022-05-18 18:39:07 -0700

    set up server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+const port = 3000;
 
+app.use(express.static('client'));
+
+app.post('/', (req, res) => {
+
+})
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+})
