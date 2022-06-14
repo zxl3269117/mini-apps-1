5dbf705b84c8b5ae13477e72d1d57287fb307cde
Ziqian Li <liziqian1992@gmail.com>
2022-06-13 22:10:09 -0700

    set up react client and express server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,19 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
 
+  render() {
+    return (
+      <div>
+        <p>Hello</p>
+      </div>
+    )
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(<App />);
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8080;
 
+app.use(express.static('node_modules'));
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.statusCode(200);
+})
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+})
