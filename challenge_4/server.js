e3d990d5d476f9bd7572b215e495cd8649fb419f
Ziqian Li <liziqian1992@gmail.com>
2022-06-20 10:51:07 -0700

    set up server using express

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 7070;
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// REQUESTS HANDLER
+app.get('/', (req, res) => {
+  // do something
+});
+
+app.listen(PORT, () => {
+  console.log(`listen on port ${PORT}`);
+})
