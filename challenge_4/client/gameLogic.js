7b92465746d2216f13c835485d66a872456428a8
Ziqian Li <liziqian1992@gmail.com>
2022-06-21 15:47:48 -0700

    build game logic to detect vertical and horizontal win

--- challenge_4/client/gameLogic.js
+++ challenge_4/client/gameLogic.js
@@ -16,8 +16,9 @@ const gameLogic = {
   detectHorizontalWin: (gameData) => {
     for(var i = 0; i < gameData.length; i++) {
       var rowData = gameData[i];
-      if(gameLogic._rowMatch(rowData)) {
-        return gameLogic._rowMatch(rowData);
+      var rowMatch = gameLogic._rowMatch(rowData);
+      if(rowMatch) {
+        return rowMatch;
       }
     }
     return null;
@@ -28,43 +29,140 @@ const gameLogic = {
     var color = '';
     var count = 0;
     for(var i = 0; i <= rowData.length; i++) {
-
-      // founc 4 connected color
+      var cell = rowData[i];
+      // found 4 connected color
       if(count === 4) {
         return color;
       }
 
       // cell is empty
-      if(rowData[i] === '') {
+      if(cell === '') {
         continue;
       } else {
 
         // if color matches
-         if(color === rowData[i]) {
+         if(color === cell) {
           count ++;
         } else {
         // if color does not match, reassign color, count start over
-          color = rowData[i];
+          color = cell;
           count = 1;
         }
+      }
+    }
+    return null;
+  },
+
+  // detect vertical win
+  detectVerticalWin: (gameData) => {
+    for(var i = 0; i < gameData[0].length; i++) {
+      var colMatch = gameLogic._colMatch(gameData, i);
+      if(colMatch) {
+        return colMatch;
+      }
+    }
+    return null;
+  },
+
+  // detect vertical match
+  _colMatch: (gameData, col) => {
+    var color = '';
+    var count = 0;
+    for(var i = 0; i < gameData.length; i++) {
+      var cell = gameData[i][col];
+      // found 4 connected color
+      if(count === 4) {
+        return color;
+      }
+
+      // cell is empty
+      if(cell === '') {
+        continue;
+      } else {
+
+        // if color matches
+         if(color === cell) {
+          count ++;
+        } else {
+        // if color does not match, reassign color, count start over
+          color = cell;
+          count = 1;
+        }
+      }
+    }
+
+    if(count === 4) {
+      return color;
+    }
+
+    return null;
+  },
+
+  // detect diagnal win
+  detectDiagnalWin: (gameData) => {
+    for(var i = 0; i < gameData[0].length; i++) {
+      var majorDiagnalMatch = gameLogic._majorDiagnalMatch(gameData, i);
+      var minorDiagnalMatch = gameLogic._minorDiagnalMatch(gameData, i);
+      if(majorDiagnalMatch || minorDiagnalMatch) {
+        return majorDiagnalMatch || minorDiagnalMatch;
       }
     }
 
     return null;
   },
 
-  // detect vertical win
-  detectVerticalWin: (gameData) => {
-
-  }
-
-  // detect vertical match
-
-  // detect diagnal win
-
   // detect major diagnal match (top left -> bottom right)
+  _majorDiagnalMatch: (gameData, col) => {
+    var color = '';
+    var count = 0;
+    var row = 0;
+    var i, j;
+
+    while(row < 3) {
+      i = row;
+      j = col;
+
+      while(gameData[i][j]) {
+        var cell = gameData[i][j];
+
+        if(count === 4) {
+          return color;
+        }
+
+        // cell is empty
+        if(cell === '') {
+          i ++;
+          j ++;
+          continue;
+        } else {
+
+          // if color matches
+          if(color === cell) {
+            count ++;
+          } else {
+          // else reassign color, count start over
+            color = cell;
+            count = 1;
+          }
+
+          i ++;
+          j ++;
+        }
+      }
+      row ++;
+    }
+
+    if(count === 4) {
+      return color;
+    }
+
+    return null;
+  },
 
   // detect minor diagnal match (top right -> bottom left)
+  _minorDiagnalMatch: (gameData, col) => {
+    return null;
+  }
 }
 
 var module = {};
--- challenge_4/spec/GameLogicSpec.js
+++ challenge_4/spec/GameLogicSpec.js
@@ -1,5 +1,7 @@
 describe('Detect win', function() {
   var detectHorizontalWin = gameLogic.detectHorizontalWin;
+  var detectVerticalWin = gameLogic.detectVerticalWin;
+  var detectDiagnalWin = gameLogic.detectDiagnalWin;
 
   describe('Empty board', function() {
     var emptyBoard = [
@@ -21,108 +23,126 @@ describe('Detect win', function() {
     });
 
     it('should detect not a major diagnal win', function() {
-      expect(detectDiagnalWin(diagnalWin)).to.equal(null);
+      expect(detectDiagnalWin(emptyBoard)).to.equal(null);
     });
 
     it('should detect not a minor diagnal win', function() {
-      expect(detectDiagnalWin(diagnalWin)).to.equal(null);
+      expect(detectDiagnalWin(emptyBoard)).to.equal(null);
     });
 
   })
 
   describe('Red winning', function() {
-    var horizontalWin = [
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['red', 'black', 'red', 'red', 'red', 'red'],
-    ];
-
     it('should detect a horizontal win', function() {
+      var horizontalWin = [
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['red', 'black', 'red', 'red', 'red', 'red'],
+      ];
       expect(detectHorizontalWin(horizontalWin)).to.equal('red');
     });
 
-    var verticalWin = [
-      ['red', '', '', '', '', ''],
-      ['red', '', '', '', '', ''],
-      ['red', '', '', '', '', ''],
-      ['red', '', '', '', '', ''],
-      ['black', '', '', '', '', ''],
-      ['black', '', '', '', '', ''],
-      ['red', '', '', '', '', ''],
-    ];
-
     it('should detect a vertical win', function() {
+      var verticalWin = [
+        ['red', '', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+        ['black', '', '', '', '', ''],
+        ['black', '', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+      ];
+
       expect(detectVerticalWin(verticalWin)).to.equal('red');
     });
 
-    var diagnalWin = [
-      ['red', '', '', '', '', ''],
-      ['', 'red', '', '', '', ''],
-      ['', '', 'red', '', '', ''],
-      ['', '', '', 'red', '', ''],
-      ['', '', '', '', '', 'black'],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-    ];
 
     it('should detect a major diagnal win', function() {
+      var diagnalWin = [
+        ['red', '', '', '', '', ''],
+        ['', 'red', '', '', '', ''],
+        ['', '', 'red', '', '', ''],
+        ['', '', '', 'red', '', ''],
+        ['', '', '', '', 'black', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+      ];
+
       expect(detectDiagnalWin(diagnalWin)).to.equal('red');
     });
 
     it('should detect a minor diagnal win', function() {
+      var diagnalWin = [
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', 'black', ''],
+        ['', '', '', 'red', '', ''],
+        ['', '', 'red', '', '', ''],
+        ['', 'red', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+      ];
       expect(detectDiagnalWin(diagnalWin)).to.equal('red');
     });
 
   })
 
   describe('Black winning', function() {
-    var horizontalWin = [
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['black', 'black', 'black', 'black', 'red', 'red'],
-    ];
 
     it('should detect a horizontal win', function(){
+      var horizontalWin = [
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['black', 'black', 'black', 'black', 'red', 'red'],
+      ];
       expect(detectHorizontalWin(horizontalWin)).to.equal('black');
     })
 
-    var verticalWin = [
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-      ['', '', '', '', '', 'red'],
-      ['', '', '', '', '', 'black'],
-      ['', '', '', '', '', 'black'],
-      ['', '', '', '', '', 'black'],
-      ['', '', '', '', '', 'black'],
-    ];
 
     it('should detect a vertical win', function() {
+      var verticalWin = [
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', 'red'],
+        ['', '', '', '', '', 'black'],
+        ['', '', '', '', '', 'black'],
+        ['', '', '', '', '', 'black'],
+        ['', '', '', '', '', 'black'],
+      ];
       expect(detectVerticalWin(verticalWin)).to.equal('black');
     });
 
-    var diagnalWin = [
-      ['', '', '', '', '', 'black'],
-      ['', '', '', '', 'black', ''],
-      ['', '', '', 'black', '', ''],
-      ['', '', 'black', '', '', ''],
-      ['', 'red', '', '', '', ''],
-      ['red', '', '', '', '', ''],
-      ['', '', '', '', '', ''],
-    ];
 
     it('should detect a major diagnal win', function() {
+      var diagnalWin = [
+        ['', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+        ['', 'red', '', '', '', ''],
+        ['', '', 'black', '', '', ''],
+        ['', '', '', 'black', '', ''],
+        ['', '', '', '', 'black', ''],
+        ['', '', '', '', '', 'black'],
+      ];
       expect(detectDiagnalWin(diagnalWin)).to.equal('black');
     });
 
     it('should detect a minor diagnal win', function() {
+      var diagnalWin = [
+        ['', '', '', '', '', 'black'],
+        ['', '', '', '', 'black', ''],
+        ['', '', '', 'black', '', ''],
+        ['', '', 'black', '', '', ''],
+        ['', 'red', '', '', '', ''],
+        ['red', '', '', '', '', ''],
+        ['', '', '', '', '', ''],
+      ];
       expect(detectDiagnalWin(diagnalWin)).to.equal('black');
     });
 
